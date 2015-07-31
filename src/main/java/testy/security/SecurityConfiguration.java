90a8e72b2799ea5bfdfbc88f9304da62d9e9484e
Andreas Roth <art_github@rtline.de>
2015-07-31 17:25:48 +0200

    aktiviert den LDAP login

--- src/main/java/testy/security/SecurityConfiguration.java
+++ src/main/java/testy/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package testy.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,19 +14,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private FakeUserDetailsService userDetailsService;
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
-	}
-
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/login").permitAll().antMatchers("/**")
 		        .fullyAuthenticated();
-		http.httpBasic();
+		http.formLogin();
 		http.csrf().disable();
+	}
+	
+	@Configuration
+	protected static class AuthenticationConfiguration extends
+			GlobalAuthenticationConfigurerAdapter {
+
+		@Override
+		public void init(AuthenticationManagerBuilder auth) throws Exception {
+			auth
+				.ldapAuthentication()
+					.userDnPatterns("uid={0}")
+					.contextSource().port(389)
+					.root("dc=intern,dc=paul-consultants,dc=de")
+					.url("ldap://intern.paul-consultants.de:389/dc=intern,dc=paul-consultants,dc=de");
+
+		}
 	}
 }
