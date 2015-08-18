8897b08e2072f2e5c03a9d13d8b6a38b642ecd4a
Andreas Roth <art_github@rtline.de>
2015-08-18 18:04:12 +0200

    Adds QuestionRepository

---
+++ src/main/java/testy/dataaccess/QuestionRepository.java
@@ -0,0 +1,13 @@
+package testy.dataaccess;
+
+import org.springframework.data.repository.CrudRepository;
+
+import testy.domain.question.AbstractQuestion;
+
+
+
+public interface QuestionRepository extends CrudRepository<AbstractQuestion<?, ?>, Long> {
+	
+	Iterable<AbstractQuestion<?, ?>> findAll();
+	
+}
