3f79cd4909d8434e896ecb20c2518d6fe789f4e2
Andreas Roth <art_github@rtline.de>
2015-08-20 17:30:51 +0200

    Adds vital fields to Subject

--- src/main/java/testy/domain/Subject.java
+++ src/main/java/testy/domain/Subject.java
@@ -5,13 +5,22 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import testy.domain.test.TestTemplate;
+import testy.domain.util.Views;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
 @Entity
 public class Subject {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@JsonView(Views.Summary.class)
 	private long id;
 	
+	@JsonView(Views.Summary.class)
 	private String name;
+	
+	private TestTemplate test;
 	
 }
