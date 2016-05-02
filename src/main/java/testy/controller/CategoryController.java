d109fec0f57c0889b07d4a32c49dcb11c8fdbce3
Andreas Roth <art_github@rtline.de>
2016-05-02 18:08:20 +0200

    Adds possibility to add questions to category

--- src/main/java/testy/controller/CategoryController.java
+++ src/main/java/testy/controller/CategoryController.java
@@ -3,13 +3,16 @@ package testy.controller;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import testy.dataaccess.CategoryRepository;
+import testy.dataaccess.QuestionRepository;
 import testy.domain.question.AbstractQuestion;
 import testy.domain.test.Category;
 import testy.domain.util.Views.Summary;
@@ -22,6 +25,9 @@ public class CategoryController extends ApiController {
 	
 	@Autowired
 	CategoryRepository catRepo;
+	
+	@Autowired
+	QuestionRepository questionRepo;
 	
 	@JsonView(Summary.class)
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -44,5 +50,18 @@ public class CategoryController extends ApiController {
 		changedCategory.setName(postedCategory.getName());
 		changedCategory = catRepo.save(changedCategory);
 		return changedCategory;
+	}
+	
+	@NeedsLoggedInAccount(admin = "true")
+	@ResponseStatus(HttpStatus.CREATED)
+	@RequestMapping(value = "/{id}/questions", method = RequestMethod.POST)
+	public AbstractQuestion addNewQuestion(@PathVariable("id") long id, @RequestBody AbstractQuestion postedQuestion) {
+		
+		Category category = catRepo.findById(id);
+		category.addQuestion(postedQuestion);
+		catRepo.save(category);
+		
+		questionRepo.save(postedQuestion);
+		return postedQuestion;	
 	}
 }
--- src/test/java/testy/controller/CategoryControllerTest.java
+++ src/test/java/testy/controller/CategoryControllerTest.java
@@ -3,17 +3,25 @@ package testy.controller;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.iterableWithSize;
+import static org.junit.Assert.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 
+import testy.domain.question.AbstractQuestion;
+import testy.domain.question.mc.MCPossibility;
+import testy.domain.question.mc.MCQuestion;
 import testy.domain.test.Category;
 import testy.helper.annotations.NeedsSessions;
 import testy.helper.annotations.NeedsTestClasses;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 public class CategoryControllerTest extends ControllerTest {
 
@@ -53,6 +61,51 @@ public class CategoryControllerTest extends ControllerTest {
 
 			// assert
 			.andExpect(jsonPath("$", iterableWithSize(testClasses.category1.getQuestions().size())));
+	}
+	
+	@NeedsTestClasses
+	@NeedsSessions
+	@Test
+	public void POST_id_questions_withoutAdminPermissions_shouldFAIL() throws Exception {
+		
+		// arrange
+		AbstractQuestion question = new MCQuestion();
+		question.setQuestionString("What the fuck is wrong with you?");
+		
+		// act
+		mockMvc
+			.perform(post("/categories/" + testClasses.category1.getId() + "/questions").session(userSession)
+			.contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(question)))
+			.andExpect(status().isForbidden());
+	}
+	
+	@NeedsTestClasses
+	@NeedsSessions
+	@Test
+	public void POST_id_questions_withAdminPermissions_shouldReturnCreatedCategory() throws Exception {
+		
+		// arrange
+		MCQuestion question = new MCQuestion();
+		question.setQuestionString("What the fuck is wrong with you?");
+		MCPossibility possibility1 = new MCPossibility("Größer", false);
+		MCPossibility possibility2 = new MCPossibility("kleiner", true);
+		question.addAnswer(possibility1);
+		question.addAnswer(possibility2);
+		mapper.setSerializationInclusion(Include.NON_NULL);
+		
+		MockHttpServletResponse response = mockMvc
+			.perform(post("/categories/" + testClasses.category1.getId() + "/questions").session(adminSession)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(mapper.writeValueAsString(question)))
+			.andExpect(status().isCreated()).andReturn().getResponse();
+
+		// assert
+		AbstractQuestion actualQuestion = mapper.readValue(response.getContentAsString(), AbstractQuestion.class);
+		assertTrue("Question string should be returned",
+			actualQuestion.getQuestionString().equals(question.getQuestionString()));
+		assertTrue("Id should be set", actualQuestion.getId() != 0);
+		assertTrue("Category should be set correctly",
+			actualQuestion.getCategory().getId() == testClasses.category1.getId());
 	}
 
 	@NeedsTestClasses
