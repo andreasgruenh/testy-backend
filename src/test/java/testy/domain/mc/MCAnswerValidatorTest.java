d46cce1bd720eff4b784a95f4974f6a11847859b
Andreas Roth <art_github@rtline.de>
2016-06-12 20:28:28 +0200

    Fixes bug

--- src/test/java/testy/domain/mc/MCAnswerValidatorTest.java
+++ src/test/java/testy/domain/mc/MCAnswerValidatorTest.java
@@ -2,12 +2,14 @@ package testy.domain.mc;
 
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.HashSet;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import scala.annotation.meta.setter;
 import testy.domain.question.mc.MCAnswer;
 import testy.domain.question.mc.MCAnswerValidator;
 import testy.domain.question.mc.MCPossibility;
@@ -44,24 +46,38 @@ public class MCAnswerValidatorTest {
 	        poss10)), new HashSet<MCPossibility>(Arrays.asList(poss7, poss8)));
 
 	@Before
-	public void setup() {
+	public void setup() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		setId(quest1, 1);
+		setId(poss1, 1);
 		quest1.addAnswer(poss1);
+		setId(poss2, 2);
 		quest1.addAnswer(poss2);
+		setId(poss3, 3);
 		quest1.addAnswer(poss3);
 
+		setId(quest2, 2);
+		setId(poss4, 4);
 		quest2.addAnswer(poss4);
+		setId(poss5, 5);
 		quest2.addAnswer(poss5);
+		setId(poss6, 6);
 		quest2.addAnswer(poss6);
 
+		setId(quest3, 3);
+		setId(poss7, 7);
 		quest3.addAnswer(poss7);
+		setId(poss8, 8);
 		quest3.addAnswer(poss8);
+		setId(poss9, 9);
 		quest3.addAnswer(poss9);
+		setId(poss10, 10);
 		quest3.addAnswer(poss10);
 	}
 
 	@Test
 	public void validate_shouldReturnMaxScoreIfEverythingIsCorrect() {
 		int score = validator.validate(answer1);
+		System.out.println(quest1.getId());
 
 		assertTrue("Validate should return maxScore if everything is correct. It was " + score
 		        + " but expected is: " + quest1.getMaxScore(), score == quest1.getMaxScore());
@@ -82,6 +98,28 @@ public class MCAnswerValidatorTest {
 		                + score + " but expected is "
 		                + (int) (quest2.getMaxScore() * (double) 1 / 3),
 		        score == (int) (quest2.getMaxScore() * (double) 1 / 3));
+	}
+	
+	private void setId(Object object, long id) throws IllegalArgumentException, IllegalAccessException {
+		Field idField = findField(object, "id");
+		boolean wasAccessible = idField.isAccessible();
+		idField.setAccessible(true);
+		idField.set(object, id);
+		idField.setAccessible(wasAccessible);
+	}
+	
+	private Field findField(Object o, String fieldName) {
+		Field field = null;
+		Class<?> currentClass = o.getClass();
+		while(field == null) {
+			try {
+				field = currentClass.getDeclaredField(fieldName);
+			} catch(Exception e) {
+				if (currentClass == null) break;
+				currentClass = currentClass.getSuperclass();
+			}
+		}
+		return field;
 	}
 
 }
